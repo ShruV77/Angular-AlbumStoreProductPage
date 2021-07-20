243f613443548cacd8b3e16039e7fbbcd93c8667
Rao <v.shruthi.rao@accenture.com>
2021-07-20 14:00:33 +0530

    Completed module3

--- src/app/album.ts
+++ src/app/album.ts
@@ -1,8 +1,9 @@
-import { Track } from "./track";
+import { Track } from './track';
+
 
 export interface Album {
-    "name": String,
-    "releaseDate": String,
-    "coverImage": String,
-    "tracks": Track[]
+  name: string;
+  releaseDate: string;
+  coverImage: string;
+  tracks: Track[];
 }
