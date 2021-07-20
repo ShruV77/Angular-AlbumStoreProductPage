eb04d4faa90d5ba9396b9134abf00a6e860a7272
Rao <v.shruthi.rao@accenture.com>
2021-07-20 13:50:17 +0530

    Completed module2

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { ProductService } from 'app/product.service';
+
+import { ProductService } from '../product.service';
 
 @Component({
   selector: 'app-product-description',
@@ -9,12 +10,11 @@ import { ProductService } from 'app/product.service';
 export class ProductDescriptionComponent implements OnInit {
 
   albumInfo;
+
   constructor(private _productService : ProductService) { }
 
   ngOnInit() {
-    this._productService.getAlbum(1).subscribe(
-      response => this.albumInfo = response
-    );
+    this._productService.getAlbum(1).subscribe( response => this.albumInfo = response);
   }
 
 }
